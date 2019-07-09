d67e8475aec54e3051bf7d6787997df32debe4c4
Ariel Hern <hernandez.ariel1228@gmail.com>
2019-07-09 14:37:41 -0400

    modified password to handle wrong selection

--- password.py
+++ password.py
@@ -1,22 +1,36 @@
 from generator import Password
 
+
+def printer():
+    print('     1.Simple Password')
+    print('     2.Strong Password')
+    print('     3.Very Strong Password')
+
+
+def pass_gen(passwrd_type):
+    try:
+        password = Password()
+        if passwrd_type == 1:
+            passwrd = password.simplePass()
+        elif passwrd_type == 2:
+            passwrd = password.strongPass()
+        elif passwrd_type == 3:
+            passwrd = password.superStrongPass()
+
+        print(f'New Password: {passwrd}')
+    except Exception as e:
+        print('Wrong input')
+        printer()
+        print('Try Again!')
+
+
 print('************************************')
 print('\t\tWelcome')
+print('\t\t  to')
+print('\tSimple Password Generator')
+print('************************************')
 print('\nPlease select the type of password')
-print('     1.Simple Password')
-print('     2.Strong Password')
-print('     3.Very Strong Password')
+printer()
 
-
-select = int(input())
-if select:
-    password = Password()
-    if select == 1:
-        passwrd = password.simplePass()
-    elif select == 2:
-        passwrd = password.strongPass()
-    elif select == 3:
-        passwrd = password.superStrongPass()
-    else:
-        print('Wrong input')
-    print('password: ' + passwrd)
+passwrd_type = int(input())
+pass_gen(passwrd_type)
